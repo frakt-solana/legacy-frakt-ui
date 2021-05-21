461966b2922ff811c2186c642294f0f507751f1c
Timur Samoylov <timikcool@yandex.ru>
2021-05-21 11:07:44 +0300

    remove program, disable strict typecheck

--- src/index.tsx
+++ src/index.tsx
@@ -3,10 +3,7 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+ReactDOM.render(<App />,
   document.getElementById("root")
 );
 
