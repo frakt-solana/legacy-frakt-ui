63f3ebd17b1e0dcecbd7db333fd4f5a3f956fd84
sablevsky <vladislav1145@gmail.com>
2021-05-24 22:48:32 +0300

    Change router

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,32 +1,38 @@
-import { HashRouter, Route, Switch } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import React from 'react'
 import { WalletProvider } from './contexts/wallet'
 import { ConnectionProvider } from './contexts/connection'
 import { AccountsProvider } from './contexts/accounts'
 import { MarketProvider } from './contexts/market'
-// import { AppLayout } from "./components/Layout";
-import AppLayout from './components/AppLayout'
 
-// import { FaucetView, HomeView } from "./views";
+import HomePage from './pages/HomePage'
+import ExplorePage from './pages/ExplorePage'
+import MyFractsPage from './pages/MyFractsPage'
 
 export function Routes() {
   return (
-    <>
-      <HashRouter basename={'/'}>
-        <ConnectionProvider>
-          <WalletProvider>
-            <AccountsProvider>
-              <MarketProvider>
-                <Switch>
-                  <Route exact path='/' component={() => <AppLayout />} />
-                  {/* <Route exact path="/" component={() => <HomeView />} />
-                      <Route exact path="/faucet" children={<FaucetView />} /> */}
-                </Switch>
-              </MarketProvider>
-            </AccountsProvider>
-          </WalletProvider>
-        </ConnectionProvider>
-      </HashRouter>
-    </>
+    <Router>
+      <ConnectionProvider>
+        <WalletProvider>
+          <AccountsProvider>
+            <MarketProvider>
+              <Switch>
+                <Route exact path='/' component={() => <HomePage />} />
+                <Route
+                  exact
+                  path='/explore'
+                  component={() => <ExplorePage />}
+                />
+                <Route
+                  exact
+                  path='/myfracts'
+                  component={() => <MyFractsPage />}
+                />
+              </Switch>
+            </MarketProvider>
+          </AccountsProvider>
+        </WalletProvider>
+      </ConnectionProvider>
+    </Router>
   )
 }
