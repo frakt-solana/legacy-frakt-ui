54818b353c77ba0a4860dd43416525f1f8077032
sablevsky <vladislav1145@gmail.com>
2021-05-24 23:48:13 +0300

    Add AppNavigation component

--- src/components/AppHeader/index.tsx
+++ src/components/AppHeader/index.tsx
@@ -2,16 +2,13 @@ import React from 'react'
 import styles from './AppHeader.module.scss'
 
 import CompanyLogo from '../CompanyLogo'
+import AppNavigation from '../AppNavigation'
 
 const AppHeader = (props: any) => {
   return (
     <div className={styles.root}>
       <CompanyLogo />
-      <ul className={styles.menuList}>
-        <li>Explore</li>
-        <li>My FRACT'S</li>
-        <li>BUTTON HERE</li>
-      </ul>
+      <AppNavigation className={styles.navigation} />
     </div>
   )
 }
---
+++ src/components/AppNavigation/index.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { NavLink } from 'react-router-dom'
+import { URLS } from '../../constants'
+
+const AppNavigation = ({ className }: any) => {
+  return (
+    <ul className={className || ''}>
+      <li>
+        <NavLink to={URLS.EXPLORE}>Explore</NavLink>
+      </li>
+      <li>
+        <NavLink to={URLS.MY_FACTS}>My FRACT'S</NavLink>
+      </li>
+      <li>BUTTON HERE</li>
+    </ul>
+  )
+}
+
+export default AppNavigation
