73c038d349b1df8b0b82e47b06faaef27c2ac20f
sablevsky <vladislav1145@gmail.com>
2021-05-24 23:48:28 +0300

    Make company logo link

--- src/components/CompanyLogo/index.tsx
+++ src/components/CompanyLogo/index.tsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom'
 import styles from './CompanyLogo.module.scss'
+import { URLS } from '../../constants'
 
 const CompanyLogo = (props: any) => {
-  return <div className={styles.root}>Fract</div>
+  return (
+    <NavLink to={URLS.ROOT} className={styles.root}>
+      Fract
+    </NavLink>
+  )
 }
 
 export default CompanyLogo
