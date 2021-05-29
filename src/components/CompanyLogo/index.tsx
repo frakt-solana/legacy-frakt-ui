251cbd760e5fe1358566e6a494207046be95bdf2
sablevsky <vladislav1145@gmail.com>
2021-05-29 19:23:57 +0300

    Change CompanyLogo component

--- src/components/CompanyLogo/index.tsx
+++ src/components/CompanyLogo/index.tsx
@@ -3,9 +3,9 @@ import { NavLink } from 'react-router-dom'
 import styles from './CompanyLogo.module.scss'
 import { URLS } from '../../constants'
 
-const CompanyLogo = (props: any) => {
+const CompanyLogo = ({ className }: any) => {
   return (
-    <NavLink to={URLS.ROOT} className={styles.root}>
+    <NavLink to={URLS.ROOT} className={`${styles.root} ${className || ''}`}>
       Fract
     </NavLink>
   )
