a4a6771d353258ae51ee64a5268efe1b4cc18a89
sablevsky <vladislav1145@gmail.com>
2021-05-29 19:26:30 +0300

    Add headerText on MyFractsPage

--- src/pages/MyFractsPage.tsx
+++ src/pages/MyFractsPage.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 import AppLayout from '../components/AppLayout'
 
 const MyFractsPage = (props: any) => {
-  return <AppLayout>MyFracts Page</AppLayout>
+  return <AppLayout headerText="My Fract's">MyFracts Page</AppLayout>
 }
 
 export default MyFractsPage
