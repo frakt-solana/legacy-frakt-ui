328445ff4f62143f72a4cef4b913a8502070a365
sablevsky <vladislav1145@gmail.com>
2021-05-29 23:27:23 +0300

    Show modal when go MyFracts without connected wallet

--- src/components/AppNavigation/index.tsx
+++ src/components/AppNavigation/index.tsx
@@ -1,9 +1,12 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom'
 import { URLS } from '../../constants'
+import { useWallet } from '../../contexts/wallet'
 import styles from './AppNavigation.module.scss'
 
 const AppNavigation = ({ className }: any) => {
+  const { connected, select } = useWallet()
+
   return (
     <ul className={`${styles.root} ${className || ''}`}>
       <li>
@@ -15,14 +18,21 @@ const AppNavigation = ({ className }: any) => {
           Explore
         </NavLink>
       </li>
+
       <li>
-        <NavLink
-          className={styles.link}
-          activeClassName={styles.activeLink}
-          to={URLS.MY_FACTS}
-        >
-          My Fract's
-        </NavLink>
+        {connected ? (
+          <NavLink
+            className={styles.link}
+            activeClassName={styles.activeLink}
+            to={URLS.MY_FACTS}
+          >
+            My Fract's
+          </NavLink>
+        ) : (
+          <p className={styles.link} onClick={select}>
+            My Fract's
+          </p>
+        )}
       </li>
     </ul>
   )
