8d9b45c0e6a3b0c48aed5629d88a624704f74f4f
sablevsky <vladislav1145@gmail.com>
2021-05-29 19:24:54 +0300

    Restyle ArtCard component

--- src/components/ArtCard/index.tsx
+++ src/components/ArtCard/index.tsx
@@ -1,13 +1,27 @@
 import React from 'react'
 import styles from './ArtCard.module.scss'
 
+const InfoTable = ({ ownerAddress, rarity }: any) => {
+  return (
+    <div className={styles.infoTable}>
+      <div>
+        <p>Owner</p>
+        <p>{truncateString(ownerAddress, 7, 4)}</p>
+      </div>
+      <div>
+        <p>Rarity</p>
+        <p>{rarity}</p>
+      </div>
+    </div>
+  )
+}
+
 const ArtCard = ({ className, onClick = () => {}, data = {} }: any) => {
   return (
     <div className={`${styles.root} ${className || ''}`} onClick={onClick}>
+      <p className={styles.title}>White portal</p>
       <img className={styles.image} src={data.imageSrc} alt='Art' />
-      <p className={styles.ownerAddress}>
-        {truncateString(data.ownerAddress, 7, 4)}
-      </p>
+      <InfoTable ownerAddress={data.ownerAddress} rarity={'10.23%'} />
     </div>
   )
 }
