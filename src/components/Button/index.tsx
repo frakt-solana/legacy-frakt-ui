ef8cd3c7dc15e3f6082015421c027871b530aa04
sablevsky <vladislav1145@gmail.com>
2021-05-29 23:10:38 +0300

    Add sm button size

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -2,15 +2,22 @@ import React from 'react'
 import { ArrowRightIcon } from '../icons'
 import styles from './Button.module.scss'
 
-const Button = ({ className, onClick = () => {}, children }: any) => {
+const Button = ({
+  className,
+  onClick = () => {},
+  size = 'md',
+  children,
+}: any) => {
   return (
     <button
       type='button'
-      className={`${styles.root} ${className || ''}`}
+      className={`${styles.root} ${
+        size === 'md' ? styles.md : size === 'sm' ? styles.sm : styles.lg
+      } ${className || ''}`}
       onClick={onClick}
     >
       {children}
-      <ArrowRightIcon className={styles.arrow} />
+      <ArrowRightIcon />
     </button>
   )
 }
