ff1f9325a2a482a877d34298f5346dad7bb409e3
sablevsky <vladislav1145@gmail.com>
2021-05-29 19:25:47 +0300

    Huge restyle of AppLayout

--- src/components/AppLayout/index.tsx
+++ src/components/AppLayout/index.tsx
@@ -1,17 +1,52 @@
 import React from 'react'
 import styles from './AppLayout.module.scss'
 
-import AppHeader from '../AppHeader'
+import CompanyLogo from '../CompanyLogo'
+import AppNavigation from '../AppNavigation'
+import { ConnectButton } from '../ConnectButton'
 import AppFooter from '../AppFooter'
 
-const AppLayout = ({children}: any) => {
+const AppLayout = ({
+  CustomHeader,
+  headerText = '',
+  children,
+  className,
+}: any) => {
   return (
-    <div className={styles.root}>
-      <AppHeader />
-      <div>{children || 'App Body here'}</div>
-      <AppFooter />
+    <div className={`${styles.root} ${className || ''}`}>
+      <Sidebar>
+        <div className={styles.logoWrapper}>
+          <CompanyLogo />
+        </div>
+        <div className={styles.profileWrapper}>
+          <ConnectButton />
+        </div>
+        <div className={styles.navigationWrapper}>
+          <AppNavigation />
+          <AppFooter className={styles.footer} />
+        </div>
+      </Sidebar>
+      <Main>
+        {CustomHeader && <CustomHeader />}
+        {headerText && <Header>{headerText}</Header>}
+        {children}
+      </Main>
     </div>
   )
 }
 
 export default AppLayout
+
+const Sidebar = ({ children, className }: any) => {
+  return (
+    <div className={`${styles.sideBar} ${className || ''}`}>{children}</div>
+  )
+}
+
+const Main = ({ children, className }: any) => {
+  return <div className={`${styles.main} ${className || ''}`}>{children}</div>
+}
+
+const Header = ({ children, className }: any) => {
+  return <div className={`${styles.header} ${className || ''}`}>{children}</div>
+}
