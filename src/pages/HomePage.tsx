1c67b7d64090f7c29c0292ba38457f53cf51dbe1
sablevsky <vladislav1145@gmail.com>
2021-06-02 17:36:12 +0300

    Implement test hero section

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,9 +1,37 @@
 import React from 'react'
 
 import AppLayout from '../components/AppLayout'
+import styles from './HomePage.module.scss'
+import { ArrowRightIcon } from '../components/icons'
+
+import PORTAl from '../mocks/images/Portal.png'
+import { Link } from 'react-router-dom'
+
+const HeroSection = () => (
+  <div
+    className={styles.heroSection}
+    style={{ backgroundImage: `url('${PORTAl}')`, backgroundSize: '80%' }}
+  >
+    <h1>Frakt</h1>
+    <p>
+      Fractal NFT's on Solana
+      <Link to='' className={styles.scrollBottomButton}>
+        <ArrowRightIcon />
+      </Link>
+    </p>
+  </div>
+)
 
 const HomePage = (props: any) => {
-  return <AppLayout>Home Page</AppLayout>
+  return (
+    <AppLayout>
+      <HeroSection />
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe iste cum
+      obcaecati voluptas veniam nisi numquam maiores beatae consequuntur
+      laudantium consequatur dolores, eveniet architecto quia fuga officiis
+      magni, at earum!
+    </AppLayout>
+  )
 }
 
 export default HomePage
