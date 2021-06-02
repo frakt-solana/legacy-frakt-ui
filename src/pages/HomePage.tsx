0354307a6158e1ddec29935d93e4e66ad3d99131
sablevsky <vladislav1145@gmail.com>
2021-06-02 18:08:51 +0300

    Add wiki link. Implement ArtExamples section

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -4,13 +4,13 @@ import AppLayout from '../components/AppLayout'
 import styles from './HomePage.module.scss'
 import { ArrowRightIcon } from '../components/icons'
 
-import PORTAl from '../mocks/images/Portal.png'
-import { HashLink as Link } from 'react-router-hash-link';
+import PORTAL from '../mocks/images/Portal.png'
+import { HashLink as Link } from 'react-router-hash-link'
 
 const HeroSection = () => (
   <div
     className={styles.heroSection}
-    style={{ backgroundImage: `url('${PORTAl}')`, backgroundSize: '80%' }}
+    style={{ backgroundImage: `url('${PORTAL}')`, backgroundSize: '80%' }}
   >
     <h1>Frakt</h1>
     <p>
@@ -24,18 +24,43 @@ const HeroSection = () => (
 
 const IntroTextSection = () => (
   <div className={styles.intoTextSection} id='introTextSection'>
-    Frakt is generative fractal art NFT collection. We are using the
-    mathematical beauty of fractals to generate stunning art and blockchain to
-    make every piece of it unique, decentralized and fun.
+    Frakt is generative{' '}
+    <a
+      href='https://en.wikipedia.org/wiki/Fractal_art'
+      target='_blank'
+      rel='noopener noreferrer'
+    >
+      fractal art
+    </a>{' '}
+    NFT collection. We are using the mathematical beauty of fractals to generate
+    stunning art and blockchain to make every piece of it unique, decentralized
+    and fun.
   </div>
 )
 
+const ArtExample = () => (
+  <div className={styles.artExample}>
+    <img src={PORTAL} alt='art' />
+  </div>
+)
+
+const ArtExamplesSection = () => (
+  <div className={styles.artExamples}>
+    <ArtExample />
+    <ArtExample />
+    <ArtExample />
+    <ArtExample />
+    <ArtExample />
+    <ArtExample />
+  </div>
+)
 
 const HomePage = (props: any) => {
   return (
     <AppLayout>
       <HeroSection />
       <IntroTextSection />
+      <ArtExamplesSection />
     </AppLayout>
   )
 }
