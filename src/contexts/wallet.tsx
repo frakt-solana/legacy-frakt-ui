846caad607d68ae92aed135e02310ea7b2b38779
sablevsky <vladislav1145@gmail.com>
2021-06-06 15:38:57 +0300

    Remove some wallets from walletsModal

--- src/contexts/wallet.tsx
+++ src/contexts/wallet.tsx
@@ -36,23 +36,23 @@ export const WALLET_PROVIDERS = [
     url: 'https://solflare.com/access-wallet',
     icon: `${ASSETS_URL}solflare.svg`,
   },
-  {
-    name: 'MathWallet',
-    url: 'https://mathwallet.org',
-    icon: `${ASSETS_URL}mathwallet.svg`,
-  },
-  {
-    name: 'Ledger',
-    url: 'https://www.ledger.com',
-    icon: `${ASSETS_URL}ledger.svg`,
-    adapter: LedgerWalletAdapter,
-  },
-  {
-    name: 'Phantom',
-    url: 'https://phantom.app/',
-    icon: `https://raydium.io/_nuxt/img/phantom.d9e3c61.png`,
-    adapter: PhantomWalletAdapter,
-  },
+  // {
+  //   name: 'MathWallet',
+  //   url: 'https://mathwallet.org',
+  //   icon: `${ASSETS_URL}mathwallet.svg`,
+  // },
+  // {
+  //   name: 'Ledger',
+  //   url: 'https://www.ledger.com',
+  //   icon: `${ASSETS_URL}ledger.svg`,
+  //   adapter: LedgerWalletAdapter,
+  // },
+  // {
+  //   name: 'Phantom',
+  //   url: 'https://phantom.app/',
+  //   icon: `https://raydium.io/_nuxt/img/phantom.d9e3c61.png`,
+  //   adapter: PhantomWalletAdapter,
+  // },
 ]
 
 export interface WalletAdapter extends EventEmitter {
