856b995d40104733a7078443eb2b132f7dd313b6
sablevsky <vladislav1145@gmail.com>
2021-07-14 01:41:55 +0300

    feature/mobile-layout - Add new Table component on RarityPage

--- src/pages/RarityPage.tsx
+++ src/pages/RarityPage.tsx
@@ -3,50 +3,37 @@ import React from 'react'
 import AppLayout from '../components/AppLayout'
 import styles from './RarityPage.module.scss'
 
-const Table = ({ head = ['Key', 'Value'], values = [] }: any) => {
-  return (
-    <div className={styles.table}>
-      <div className={styles.tableHead}>
-        <p>{head[0]}</p>
-        <p>{head[1]}</p>
-      </div>
-      {values.map(([key, value]) => (
-        <div>
-          <p>{key}</p>
-          <p>{value}</p>
-        </div>
-      ))}
-    </div>
-  )
-}
+import Table from '../components/Table'
 
-const RarityPage = (props: any) => {
+const RarityPage = () => {
   return (
     <AppLayout headerText=''>
       <div className={styles.root}>
         <div className={styles.tableContainer}>
           <h2>Figure rarity</h2>
           <Table
-            head={['Figure', 'Rarity']}
-            values={[
+            header={['Figure', 'Rarity']}
+            data={[
               ['Net', '44%'],
               ['Portal', '30%'],
               ['Star', '20%'],
               ['Eye', '5%'],
               ['Wave', '1%'],
             ]}
+            size='md'
           />
         </div>
         <div className={styles.tableContainer}>
           <h2>Color rarity</h2>
           <Table
-            head={['Color', 'Rarity']}
-            values={[
+            header={['Color', 'Rarity']}
+            data={[
               ['White', '40%'],
               ['Orange', '30%'],
               ['Red', '20%'],
               ['Magenta/Rainbow', '10%'],
             ]}
+            size='md'
           />
         </div>
       </div>
