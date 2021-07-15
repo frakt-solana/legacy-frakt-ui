4a618945b9ccb8112ed21762ad0f3eb0a2d4f61b
sablevsky <vladislav1145@gmail.com>
2021-07-16 00:23:28 +0300

    feature/mobile-layout - Implement useLazyArtImageSrc hook

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,5 +1,27 @@
+import { useState } from 'react'
+import { ipfsUriToGatewayUrl } from '../utils/ipfs'
+
 export * from './useUserAccounts'
 export * from './useAccountByMint'
 export * from './useTokenName'
 export * from './useUserBalance'
 export * from './useUserTotalBalance'
+
+export const useLazyArtImageSrc = () => {
+  const [src, setSrc] = useState(null)
+  const [loading, setLoading] = useState(false)
+  const [error, setError] = useState(null)
+
+  const getSrc = (imageUrl) => {
+    setLoading(true)
+    fetch(ipfsUriToGatewayUrl(imageUrl))
+      .then((res) => res.json())
+      .then(({ image }) => {
+        setSrc(ipfsUriToGatewayUrl(image))
+      })
+      .catch((err) => setError(err))
+      .finally(() => setLoading(false))
+  }
+
+  return { src, loading, error, getSrc }
+}
