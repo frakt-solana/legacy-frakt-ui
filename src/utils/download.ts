5359d3a7c6c86b56c44c4b7c2b669b596d97f61c
sablevsky <vladislav1145@gmail.com>
2021-07-20 20:16:49 +0300

    feature/download-high-quality - Remove download.ts file from utils folder

--- src/utils/download.ts
+++
@@ -1,6 +0,0 @@
-export const openDownloadLink = (url, filename) => {
-  const downloadLink = document.createElement('a')
-  downloadLink.href = url
-  filename && (downloadLink.download = filename)
-  downloadLink.click()
-}
