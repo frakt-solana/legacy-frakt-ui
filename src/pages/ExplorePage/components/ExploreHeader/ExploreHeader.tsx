eb27dc5787b69a1fceae76cbfa0fb672171b46f1
sablevsky <vladislav1145@gmail.com>
2021-07-21 21:35:53 +0300

    feature/share-collection - Implement ExploreHeader component

---
+++ src/pages/ExplorePage/components/ExploreHeader/ExploreHeader.tsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import { PublicKey } from '@solana/web3.js'
+
+import styles from './styles.module.scss'
+import { getHeaderText } from '../../helpers'
+import CopyURLButton from '../../../../components/CopyURLButton'
+import TwitterShareButton from '../../../../components/TwitterShareButton'
+
+interface IExploreHeaderProps {
+  userAddress?: string
+  walletKey?: PublicKey
+}
+
+const TWEET_TEXT =
+  'Look at my collection of #frakts so far!\n\nEach #NFT was randomly generated with various rarities. Do you see any you like?'
+const HASHTAGS = ['NFTs', 'digitalart', 'NFTcollector', 'NFTart']
+
+const ExploreHeader = ({ userAddress, walletKey }: IExploreHeaderProps) => (
+  <div className={styles.root}>
+    <p className={styles.text}>{getHeaderText({ userAddress, walletKey })}</p>
+    {!!userAddress && (
+      <div className={styles.buttonsWrapper}>
+        <CopyURLButton size='md' />
+        <TwitterShareButton
+          size='md'
+          tweetText={TWEET_TEXT}
+          hashtags={HASHTAGS}
+        />
+      </div>
+    )}
+  </div>
+)
+
+export default ExploreHeader
