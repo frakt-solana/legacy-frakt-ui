ed1fa448c58f9a3b8f13451db91f7f59742d9338
sablevsky <vladislav1145@gmail.com>
2021-07-27 19:00:11 +0300

    feature/pre-update-fixes - Change counter value

--- src/pages/HomePage/sections/BuySection/BuySection.tsx
+++ src/pages/HomePage/sections/BuySection/BuySection.tsx
@@ -1,19 +1,19 @@
 import React from 'react'
 
 import styles from './styles.module.scss'
-import { useArts } from '../../../../contexts/artDetails'
+// import { useArts } from '../../../../contexts/artDetails'
 import BuyButton from '../../../../components/BuyButton'
 import Table from '../../../../components/Table'
 
 const BuySection = () => {
-  const { counter, arts } = useArts()
+  // const { counter, arts } = useArts()
   return (
     <div className={styles.root}>
       <div className={styles.content}>
         <Table
           className={styles.table}
           data={[
-            ['Generated', `${counter || arts?.length || 0} / 10 000`],
+            ['Generated', `10 000 / 10 000`],
             ['Price', '0.5 SOL'],
           ]}
         />
