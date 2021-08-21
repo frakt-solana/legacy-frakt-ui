2d7a43b50b8492e23774e5210ca11037651662c5
Timikcool <timikcool@yandex.ru>
2021-08-21 20:20:00 +0400

    remove akash section

--- src/pages/HomePage/sections/FooterSection/FooterSection.tsx
+++ src/pages/HomePage/sections/FooterSection/FooterSection.tsx
@@ -9,16 +9,7 @@ const FooterSection = () => (
       Disclaimer: FRAKT is an art experiment with an unaudited smart contract
       aimed at showcasing the nature of fractals and power of the Solana blockchain.
     </p>
-    <p className={styles.hosted}>
-      Hosted on{' '}
-      <a
-        href='https://akash.network/'
-        target='_blank'
-        rel='noopener noreferrer'
-      >
-        Akash
-      </a>
-    </p>
+
   </div>
 )
 
