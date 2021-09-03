54ed2119f28710eb08d4542e3255cf414057c323
sablevsky <vladislav1145@gmail.com>
2021-09-03 16:31:11 +0300

    Add level on user explore page

---
+++ src/pages/ExplorePage/components/UserLevel/UserLevel.tsx
@@ -0,0 +1,27 @@
+import React from 'react'
+
+import styles from './styles.module.scss'
+
+interface IUserLevelProps {
+  points: number
+}
+
+const getLevel = (points) => {
+  if (points < 100) return 1
+  else if (points < 500) return 2
+  else if (points < 1000) return 3
+  else if (points < 2500) return 4
+  else return 5
+}
+
+const UserLevel = ({ points }: IUserLevelProps) => {
+  const level = getLevel(points)
+
+  return (
+    <div className={styles.root}>
+      Launchpd: <b>{points}</b> points | LEVEL <b>{level}</b>
+    </div>
+  )
+}
+
+export default UserLevel
