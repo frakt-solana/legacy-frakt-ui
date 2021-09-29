2413417075188a56a3f9ff35dcbe43476f40d0d7
sablevsky <vladislav1145@gmail.com>
2021-09-29 16:01:52 +0400

    Add page title for 404 page

--- src/pages/Page404/Page404.tsx
+++ src/pages/Page404/Page404.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
+import { Helmet } from 'react-helmet';
 
 import AppLayout from '../../components/AppLayout';
 import styles from './styles.module.scss';
 
 const Page404 = (): JSX.Element => (
   <AppLayout>
+    <Helmet>
+      <title>{`Not found | FRAKT: Generative Art NFT Collection on Solana`}</title>
+    </Helmet>
     <div className={styles.root}>
       <h1 className={styles.title}>404</h1>
       <p className={styles.subtitle}>This page does not exist</p>
