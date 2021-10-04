d15060334bbf12642dadbe56bcb87ad284300677
sablevsky <vladislav1145@gmail.com>
2021-10-04 13:09:14 +0400

    Add base layout for staking page

--- src/pages/StakePages/StakingPage.tsx
+++ src/pages/StakePages/StakingPage.tsx
@@ -9,29 +9,50 @@ import { URLS } from '../../constants';
 import styles from './styles.module.scss';
 
 const StakingPage = (): JSX.Element => (
-  <AppLayout headerText="Staking">
+  <AppLayout headerText="Staking" mainClassName={styles.appMain}>
     <Helmet>
       <title>{`Staking | FRAKT: Generative Art NFT Collection on Solana`}</title>
     </Helmet>
     <div className={styles.stakingPage}>
-      <p>You are staking 10 Frakts</p>
-
-      <NavLink to={URLS.STAKING_CREATE}>
-        <Button size="lg">Stake</Button>
-      </NavLink>
-      <NavLink to={URLS.STAKING_UNSTAKE}>
-        <Button size="md">Unstake</Button>
-      </NavLink>
-
-      <Table
-        data={[
-          ['Frakts staking', '10'],
-          ['Points staking', '280'],
-          ['FRKTs to harvest', 50],
-        ]}
-      />
-
-      <Button>Harvest</Button>
+      <div className={styles.stakingPage__stakeWrapper}>
+        <Table
+          size="md"
+          data={[
+            ['Frakts to stake', '100'],
+            ['Points to stake', '500'],
+          ]}
+          className={styles.stakingPage__infoTable}
+        />
+        <NavLink to={URLS.STAKING_CREATE}>
+          <Button size="md">Stake</Button>
+        </NavLink>
+      </div>
+      <div className={styles.stakingPage__harvestWrapper}>
+        <Table
+          size="lg"
+          data={[
+            ['Points staking', '280'],
+            ['FRKTs per month', '100'],
+            ['FRKTs earned', '500'],
+          ]}
+          className={styles.stakingPage__infoTable}
+        />
+        <Button size="lg">Harvest</Button>
+      </div>
+      <div className={styles.stakingPage__unstakeWrapper}>
+        <Table
+          size="md"
+          data={[
+            ['Frakts staking', '10'],
+            ['Points staking', '280'],
+            ['Available to unstake', '5'],
+          ]}
+          className={styles.stakingPage__infoTable}
+        />
+        <NavLink to={URLS.STAKING_UNSTAKE}>
+          <Button size="md">Unstake 5</Button>
+        </NavLink>
+      </div>
     </div>
   </AppLayout>
 );
