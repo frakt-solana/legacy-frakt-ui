a5854fadc94dc8159f3c6d107760801c151bd56a
sablevsky <vladislav1145@gmail.com>
2021-10-04 13:08:28 +0400

    Make staking page private

--- src/components/AppNavigation/AppNavigation.tsx
+++ src/components/AppNavigation/AppNavigation.tsx
@@ -3,17 +3,25 @@ import React from 'react';
 import { URLS } from '../../constants';
 import styles from './styles.module.scss';
 import { NavigationLink } from './NavigationLink';
+import { useWallet } from '../../external/contexts/wallet';
 
 interface AppNavigation {
   className?: string;
 }
 
 const AppNavigation = ({ className }: AppNavigation): JSX.Element => {
+  const { connected, select } = useWallet();
+
   return (
     <ul className={`${styles.root} ${className || ''}`}>
       <NavigationLink to={URLS.COLLECTION} text="Collection" />
       <NavigationLink to={URLS.RARITY} text="Rarity hdbk" />
-      <NavigationLink to={URLS.STAKING} text="Staking" />
+      <NavigationLink
+        to={URLS.STAKING}
+        text="Staking"
+        notLink={!connected}
+        onClick={select}
+      />
       <NavigationLink to={URLS.MARKETPLACE} text="Marketplace" />
       <li>
         <a className={styles.link} href={URLS.LAUNCHPAD}>
