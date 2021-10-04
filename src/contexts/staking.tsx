8a194cdef250b8f96c38f9a6958c2a8e652accc8
sablevsky <vladislav1145@gmail.com>
2021-10-04 22:58:55 +0400

    Add userFrakts and stake func in staking provider

--- src/contexts/staking.tsx
+++ src/contexts/staking.tsx
@@ -3,20 +3,26 @@ import { PublicKey } from '@solana/web3.js';
 import * as contract from 'frakt-client';
 import { useConnection } from '../external/contexts/connection';
 import config from '../config';
+import { useWallet } from '../external/contexts/wallet';
+import { notify } from '../external/utils/notifications';
+import { Frakt } from './frakts';
 
 const programPubKey = new PublicKey(config.PROGRAM_PUBLIC_KEY);
 
 interface StakingContextInterface {
   poolConfigAccount: contract.PoolConfigView;
   stakeAccounts: contract.StakeView[];
+  userFrakts: Frakt[];
   loading: boolean;
   error: Error | null;
   fetchData: () => Promise<void>;
+  stakeFrakts: (artsAndMints: contract.ArtAndMint[]) => Promise<boolean>;
 }
 
 export const StakingContext = React.createContext({
   poolConfigAccount: null,
   stakeAccounts: [],
+  userFrakts: [],
   loading: false,
   error: null,
   fetchData: async () => {},
@@ -31,16 +37,26 @@ export const StakingProvider = ({ children = null as any }): JSX.Element => {
   const [poolConfigAccount, setPoolConfigAccount] =
     useState<contract.PoolConfigView>(null);
   const [stakeAccounts, setStakeAccounts] = useState<contract.StakeView[]>([]);
+  const [userFrakts, setUserFrakts] = useState<Frakt[]>([]);
 
   const connection = useConnection();
+  const { wallet } = useWallet();
 
   const fetchData = async (): Promise<void> => {
     if (loading) return;
     try {
       setLoading(true);
-      const { stakeAccounts, poolConfigAccount } =
+      const { stakeAccounts, poolConfigAccount, artAccounts } =
         await contract.getAllProgramAccounts(programPubKey, { connection });
+      const tokens = await contract.getAllUserTokens(wallet.publicKey, {
+        connection,
+      });
+      const tokensMint = tokens.map(({ mint }) => mint);
+      const userFrakts = artAccounts.filter((frakt) => {
+        return tokensMint.includes(frakt?.metadata?.minted_token_pubkey);
+      });
 
+      setUserFrakts(userFrakts as Frakt[]);
       setPoolConfigAccount(poolConfigAccount);
       setStakeAccounts(stakeAccounts);
     } catch (error) {
@@ -50,9 +66,51 @@ export const StakingProvider = ({ children = null as any }): JSX.Element => {
     }
   };
 
+  const stakeFrakts = async (
+    artsAndMints: contract.ArtAndMint[],
+  ): Promise<boolean> => {
+    try {
+      void (await contract.stakeBulkFrakts(
+        artsAndMints,
+        wallet.publicKey,
+        programPubKey,
+        async (txn) => {
+          const { blockhash } = await connection.getRecentBlockhash();
+
+          txn.recentBlockhash = blockhash;
+          txn.feePayer = wallet.publicKey;
+          const signed = await wallet.signTransaction(txn);
+          const txid = await connection.sendRawTransaction(signed.serialize());
+          return void connection.confirmTransaction(txid);
+        },
+        { connection },
+      ));
+      notify({
+        message: `${artsAndMints.length} Frakts staked successfully`,
+        type: 'success',
+      });
+      return true;
+    } catch (error) {
+      notify({
+        message: 'Transaction failed',
+        type: 'error',
+      });
+      // eslint-disable-next-line no-console
+      console.log(error);
+    }
+  };
+
   return (
     <StakingContext.Provider
-      value={{ poolConfigAccount, stakeAccounts, loading, error, fetchData }}
+      value={{
+        poolConfigAccount,
+        stakeAccounts,
+        loading,
+        error,
+        fetchData,
+        stakeFrakts,
+        userFrakts,
+      }}
     >
       {children}
     </StakingContext.Provider>
@@ -60,13 +118,22 @@ export const StakingProvider = ({ children = null as any }): JSX.Element => {
 };
 
 export const useStaking = (): StakingContextInterface => {
-  const { poolConfigAccount, stakeAccounts, loading, error, fetchData } =
-    useContext(StakingContext);
+  const {
+    poolConfigAccount,
+    stakeAccounts,
+    loading,
+    error,
+    fetchData,
+    stakeFrakts,
+    userFrakts,
+  } = useContext(StakingContext);
   return {
     poolConfigAccount,
     stakeAccounts,
     loading,
     error,
     fetchData,
+    stakeFrakts,
+    userFrakts,
   };
 };
