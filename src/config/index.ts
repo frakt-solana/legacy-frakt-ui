f928f879902356a503960762d41e241bbac8cef7
sablevsky <vladislav1145@gmail.com>
2021-10-30 01:30:36 +0300

    Change solana node

--- src/config/index.ts
+++ src/config/index.ts
@@ -6,7 +6,8 @@ export type ENV = 'mainnet-beta' | 'testnet' | 'devnet' | 'localnet';
 export const ENDPOINTS = [
   {
     name: 'mainnet-beta' as ENV,
-    endpoint: 'https://api.metaplex.solana.com/',
+    endpoint:
+      'https://wild-muddy-dream.solana-mainnet.quiknode.pro/47edfe015adbb24664df02bc436d4cb6272dc01a/',
     chainID: ChainID.MainnetBeta,
   },
   {
