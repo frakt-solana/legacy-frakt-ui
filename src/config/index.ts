cbe4d2db6cbcb0588a618c4bf3a6f90fc23cbb5e
sablevsky <vladislav1145@gmail.com>
2021-11-06 00:16:41 +0300

    Change mainnet node

--- src/config/index.ts
+++ src/config/index.ts
@@ -6,9 +6,10 @@ export type ENV = 'mainnet-beta' | 'testnet' | 'devnet' | 'localnet';
 export const ENDPOINTS = [
   {
     name: 'mainnet-beta' as ENV,
-    endpoint: 'https://api.metaplex.solana.com/',
+    // endpoint: 'https://api.metaplex.solana.com/',
     // endpoint:
     //   'https://wild-muddy-dream.solana-mainnet.quiknode.pro/47edfe015adbb24664df02bc436d4cb6272dc01a/',
+    endpoint: 'https://connect.runnode.com/?apikey=zzyoGRqbpZqtGZexDHfV',
     chainID: ChainID.MainnetBeta,
   },
   {
