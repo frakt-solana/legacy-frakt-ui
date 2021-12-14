2f4de5a9b35018a405cbdc264618c40cbf938a99
Kirill Dryga <webpkt@gmail.com>
2021-12-14 22:01:19 +0300

    Remove external

--- src/external/hooks/useUserAccounts.ts
+++
@@ -1,9 +0,0 @@
-import { TokenAccount } from '../models';
-import { useAccountsContext } from './../contexts/accounts';
-
-export function useUserAccounts(): { userAccounts: TokenAccount[] } {
-  const context = useAccountsContext();
-  return {
-    userAccounts: context.userAccounts as TokenAccount[],
-  };
-}
