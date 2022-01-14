932dd88c1dc0e793b1d0b5d884a6dbc6891481db
Roman Dogoda <piterom911@gmail.com>
2022-01-14 17:59:09 +0200

    Create Team section

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -14,7 +14,19 @@ import { CustomHeader } from './CustomHeader';
 import rainbowWaveImage from './assets/images/rainbowWave.jpg';
 import fraktionArtImage from './assets/images/fraktion.art.png';
 import howWorkImage from './assets/images/howWork.jpg';
-import { ArrowRightIcon, ArrowRightTop } from '../../icons';
+import teamPhotoTim from './assets/images/team/teamPhotoTim.jpg';
+import teamPhotoPhil from './assets/images/team/teamPhotoPhil.jpg';
+import teamPhotoSid from './assets/images/team/teamPhotoSid.jpg';
+import teamPhotoVlad from './assets/images/team/teamPhotoVlad.jpg';
+import teamPhotoRoman from './assets/images/team/teamPhotoRoman.jpg';
+import teamPhotoVedamire from './assets/images/team/teamPhotoVedamire.jpg';
+import teamPhotoViktor from './assets/images/team/teamPhotoViktor.jpg';
+import {
+  GitHubIcon,
+  TwitterIcon,
+  ArrowRightIcon,
+  ArrowRightTop,
+} from '../../icons';
 import { Swiper, SwiperSlide } from 'swiper/react/swiper-react';
 import 'swiper/swiper.min.css';
 import 'swiper/modules/navigation/navigation.scss';
@@ -275,6 +287,112 @@ const HomePage = (): JSX.Element => {
       <section className={styles.team}>
         <div className={`container ${styles.teamContainer}`}>
           <h2 className={styles.teamTitle}>Meet the team</h2>
+          <ul className={styles.teamList}>
+            <li className={styles.teamItem}>
+              <img src={teamPhotoTim} alt="Tim" className={styles.teamPhoto} />
+              <div className={styles.teamInfo}>
+                <p className={styles.teamName}>Tim</p>
+                <div className={styles.teamPosition}>
+                  <span>CEO</span>
+                  <a href="/" target="_blank">
+                    <TwitterIcon width={24} />
+                  </a>
+                </div>
+              </div>
+            </li>
+            <li className={styles.teamItem}>
+              <img
+                src={teamPhotoVedamire}
+                alt="Vedamire"
+                className={styles.teamPhoto}
+              />
+              <div className={styles.teamInfo}>
+                <p className={styles.teamName}>Vedamire</p>
+                <div className={styles.teamPosition}>
+                  <span>CTO</span>
+                  <a href="/" target="_blank">
+                    <GitHubIcon width={24} />
+                  </a>
+                </div>
+              </div>
+            </li>
+            <li className={styles.teamItem}>
+              <img
+                src={teamPhotoVlad}
+                alt="Vlad"
+                className={styles.teamPhoto}
+              />
+              <div className={styles.teamInfo}>
+                <p className={styles.teamName}>Vlad</p>
+                <div className={styles.teamPosition}>
+                  <span>CDO, UX Lead</span>
+                  <a href="/" target="_blank">
+                    <GitHubIcon width={24} />
+                  </a>
+                </div>
+              </div>
+            </li>
+            <li className={styles.teamItem}>
+              <img
+                src={teamPhotoPhil}
+                alt="Phil"
+                className={styles.teamPhoto}
+              />
+              <div className={styles.teamInfo}>
+                <p className={styles.teamName}>Phil</p>
+                <div className={styles.teamPosition}>
+                  <span>COO</span>
+                  <a href="/" target="_blank">
+                    <TwitterIcon width={24} />
+                  </a>
+                </div>
+              </div>
+            </li>
+            <li className={styles.teamItem}>
+              <img
+                src={teamPhotoViktor}
+                alt="Viktor"
+                className={styles.teamPhoto}
+              />
+              <div className={styles.teamInfo}>
+                <p className={styles.teamName}>Viktor</p>
+                <div className={styles.teamPosition}>
+                  <span>Developer</span>
+                  <a href="/" target="_blank">
+                    <GitHubIcon width={24} />
+                  </a>
+                </div>
+              </div>
+            </li>
+            <li className={styles.teamItem}>
+              <img src={teamPhotoSid} alt="Sid" className={styles.teamPhoto} />
+              <div className={styles.teamInfo}>
+                <p className={styles.teamName}>Sid</p>
+                <div className={styles.teamPosition}>
+                  <span>Art Director</span>
+                  <a href="/" target="_blank">
+                    <TwitterIcon width={24} />
+                  </a>
+                </div>
+              </div>
+            </li>
+            <li className={styles.teamItem}>
+              <img
+                src={teamPhotoRoman}
+                alt="Roman"
+                className={styles.teamPhoto}
+              />
+              <div className={styles.teamInfo}>
+                <p className={styles.teamName}>Roman</p>
+                <div className={styles.teamPosition}>
+                  <span>Developer</span>
+                  <a href="/" target="_blank">
+                    <GitHubIcon width={24} />
+                  </a>
+                </div>
+              </div>
+            </li>
+          </ul>
         </div>
       </section>
 
