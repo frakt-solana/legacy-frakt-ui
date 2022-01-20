a727fe7bdbe6e534cd81cf470aba399650d5fe71
Roman Dogoda <piterom911@gmail.com>
2022-01-20 11:00:23 +0200

    Fix styles and container size in Rarity hdbk Page

--- src/pages/RarityPage/RarityPage.tsx
+++ src/pages/RarityPage/RarityPage.tsx
@@ -6,37 +6,39 @@ import AppLayout from '../../components/AppLayout';
 import styles from './styles.module.scss';
 
 const RarityPage = (): JSX.Element => (
-  <AppLayout headerText="Rarity handbook">
-    <Helmet>
-      <title>{`Rarity | FRAKT: Generative Art NFT Collection on Solana`}</title>
-    </Helmet>
-    <div className={styles.root}>
-      <div className={styles.tableContainer}>
-        <Table
-          className={styles.table}
-          header={['Figure', 'Rarity']}
-          data={[
-            ['Net', '44%'],
-            ['Portal', '30%'],
-            ['Star', '20%'],
-            ['Eye', '5%'],
-            ['Wave', '1%'],
-          ]}
-          size="md"
-        />
-      </div>
-      <div className={styles.tableContainer}>
-        <Table
-          className={styles.table}
-          header={['Color', 'Rarity']}
-          data={[
-            ['White', '40%'],
-            ['Orange', '30%'],
-            ['Red', '20%'],
-            ['Magenta/Rainbow', '10%'],
-          ]}
-          size="md"
-        />
+  <AppLayout>
+    <div className="container">
+      <Helmet>
+        <title>{`Rarity | FRAKT: Generative Art NFT Collection on Solana`}</title>
+      </Helmet>
+      <div className={styles.root}>
+        <div className={styles.tableContainer}>
+          <Table
+            className={styles.table}
+            header={['Figure', 'Rarity']}
+            data={[
+              ['Net', '44%'],
+              ['Portal', '30%'],
+              ['Star', '20%'],
+              ['Eye', '5%'],
+              ['Wave', '1%'],
+            ]}
+            size="md"
+          />
+        </div>
+        <div className={styles.tableContainer}>
+          <Table
+            className={styles.table}
+            header={['Color', 'Rarity']}
+            data={[
+              ['White', '40%'],
+              ['Orange', '30%'],
+              ['Red', '20%'],
+              ['Magenta/Rainbow', '10%'],
+            ]}
+            size="md"
+          />
+        </div>
       </div>
     </div>
   </AppLayout>
