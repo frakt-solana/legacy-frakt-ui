3a5cc104708bb022f0542e38c65c0cbaba9d4a6c
Roman Dogoda <piterom911@gmail.com>
2022-02-01 12:32:48 +0200

    Create Pools list

--- src/pages/MarketPage/MarketPage.tsx
+++ src/pages/MarketPage/MarketPage.tsx
@@ -4,12 +4,86 @@ import { Helmet } from 'react-helmet';
 import AppLayout from '../../components/AppLayout';
 import styles from './styles.module.scss';
 import classNames from 'classnames';
-import { ArrowDownSmallIcon, ArrowRightTop, SwapIcon } from '../../icons';
+import {
+  ArrowDownSmallIcon,
+  ArrowRightTop,
+  SolanaIcon,
+  SwapIcon,
+} from '../../icons';
 import { NavLink } from 'react-router-dom';
 import { Input } from 'antd';
 import { SearchOutlined } from '@ant-design/icons';
 import { ControlledSelect } from '../../components/Select/Select';
 import { useForm } from 'react-hook-form';
+
+import tempImage from '../../images/examples/collectionCingular.png';
+
+const POOLS_DATA = [
+  {
+    poolImage: tempImage,
+    collectionsAmount: 123,
+    itemsAmount: 123124,
+    tokenName: 'PUNKS',
+    tokenImage: tempImage,
+    price: '0.000',
+  },
+  {
+    poolImage: tempImage,
+    collectionsAmount: 123,
+    itemsAmount: 123124,
+    tokenName: 'PUNKS',
+    tokenImage: tempImage,
+    price: '0.000',
+  },
+  {
+    poolImage: tempImage,
+    collectionsAmount: 123,
+    itemsAmount: 123124,
+    tokenName: 'PUNKS',
+    tokenImage: tempImage,
+    price: '0.000',
+  },
+  {
+    poolImage: tempImage,
+    collectionsAmount: 123,
+    itemsAmount: 123124,
+    tokenName: 'PUNKS',
+    tokenImage: tempImage,
+    price: '0.000',
+  },
+  {
+    poolImage: tempImage,
+    collectionsAmount: 123,
+    itemsAmount: 123124,
+    tokenName: 'PUNKS',
+    tokenImage: tempImage,
+    price: '0.000',
+  },
+  {
+    poolImage: tempImage,
+    collectionsAmount: 123,
+    itemsAmount: 123124,
+    tokenName: 'PUNKS',
+    tokenImage: tempImage,
+    price: '0.000',
+  },
+  {
+    poolImage: tempImage,
+    collectionsAmount: 123,
+    itemsAmount: 123124,
+    tokenName: 'PUNKS',
+    tokenImage: tempImage,
+    price: '0.000',
+  },
+  {
+    poolImage: tempImage,
+    collectionsAmount: 123,
+    itemsAmount: 123124,
+    tokenName: 'PUNKS',
+    tokenImage: tempImage,
+    price: '0.000',
+  },
+];
 
 const SORT_VALUES = [
   {
@@ -178,6 +252,42 @@ const MarketPage = (): JSX.Element => {
                 />
               </div>
             </div>
+            <ul className={styles.poolsList}>
+              {POOLS_DATA.map((item, index) => (
+                <li key={index} className={styles.poolCard}>
+                  <div className={styles.poolImgWrapper}>
+                    <img
+                      src={item.poolImage}
+                      alt="pool card"
+                      className={styles.poolImage}
+                    />
+                    <div className={styles.poolShadow}>
+                      <p className={styles.poolInfoLabel}>
+                        {item.collectionsAmount} collections
+                      </p>
+                      <p className={styles.poolInfoLabel}>
+                        {item.itemsAmount} items
+                      </p>
+                    </div>
+                  </div>
+                  <div className={styles.cardContentWrapper}>
+                    <div className={styles.poolTokenInfo}>
+                      <div
+                        className={styles.tokenImage}
+                        style={{ backgroundImage: `url(${item.tokenImage})` }}
+                      />
+                      <p className={styles.tokenName}>{item.tokenName}</p>
+                    </div>
+                    <span className={styles.priceLabel}>price</span>
+                    <div className={styles.priceWrapper}>
+                      <span className={styles.poolPrice}>{item.price}</span>
+                      <SolanaIcon />
+                      <span className={styles.priceCurrency}>SOL</span>
+                    </div>
+                  </div>
+                </li>
+              ))}
+            </ul>
           </div>
         </div>
       </div>
